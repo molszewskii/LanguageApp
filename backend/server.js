a9b9ee262f64fe7eb00154192c5f19e427493001
Michał <michal.olszewski@vip.onet.pl>
2024-12-02 18:50:50 +0100

    Set up route for levels API in server.js

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,9 @@ mongoose.connect(process.env.MONGO_URI)
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", ()=>console.log("Connected to MongoDB")); 
+
+const levelRouter = require("./routes/levels");
+app.use("/api/levels",levelRouter);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))

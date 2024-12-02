66503f28d6c2e4a68103fe6193ea720c2e6cf7ea
Michał <michal.olszewski@vip.onet.pl>
2024-12-02 18:27:12 +0100

    Set up MongoDB connection with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch((error) => console.error("MongoDb conntection error:", error));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=>console.log("Connected to MongoDB")); 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))

873a47d6b07b88bd3a8211996bc5d84a65231442
Michał <michal.olszewski@vip.onet.pl>
2024-12-03 22:07:16 +0100

    Add route for getAllLevels endpoint

--- backend/routes/levels.js
+++ backend/routes/levels.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const levelController = require("../controllers/levelController");
 
 router.post("/createLevel", levelController.createLevel);
-
+router.get("/getAllLevels", levelController.getAllLevels);
 module.exports = router;

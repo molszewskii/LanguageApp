cd756cdd46c3835e265407a8a82fbd0451b83b0e
Michał <michal.olszewski@vip.onet.pl>
2024-12-03 22:29:22 +0100

    Implement deleteLevel endpoint to delete level data

--- backend/controllers/levelController.js
+++ backend/controllers/levelController.js
@@ -71,3 +71,15 @@ exports.updateLevel = async(req,res)=>{
         res.status(500).json({message: error.message});
     }
 }
+
+exports.deleteLevel = async(req,res)=>{
+    try{
+        const levelToDelete = await Level.findByIdAndDelete(req.params.id);
+        if(!levelToDelete){
+            return res.status(404).json({message: "Level not found"});
+        }
+        res.status(200).json({message: "Level deleted successfully"});
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+}

ed8a6df986643b84a4c56ebeb38a72d4f4a54d65
Michał <michal.olszewski@vip.onet.pl>
2024-12-06 23:35:29 +0100

    Add route for deleteQuestion endpoint

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -6,4 +6,5 @@ router.post("/createQuestion", questionController.createQuestion);
 router.get("/getQuestionsByLevel/:levelId", questionController.getQuestionsByLevel);
 router.get("/getAllQuestions", questionController.getAllQuestions);
 router.get("/getQuestionById/:id", questionController.getQuestionById);
+router.delete("/deleteQuestion/:id", questionController.deleteQuestion);
 module.exports = router;

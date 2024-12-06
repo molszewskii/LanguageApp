74208ef67d2a35e3ceefa3942616f1920e26a1f4
Michał <michal.olszewski@vip.onet.pl>
2024-12-06 23:29:44 +0100

    Add route for getQuestionById endpoint

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -5,4 +5,5 @@ const questionController = require("../controllers/questionController")
 router.post("/createQuestion", questionController.createQuestion);
 router.get("/getQuestionsByLevel/:levelId", questionController.getQuestionsByLevel);
 router.get("/getAllQuestions", questionController.getAllQuestions);
+router.get("/getQuestionById/:id", questionController.getQuestionById);
 module.exports = router;

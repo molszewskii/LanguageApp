19bd5a847725c1d19dae56111a03830ee6dac299
Michał <michal.olszewski@vip.onet.pl>
2024-12-10 20:31:29 +0100

    Add loginUser endpoint to log in user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 
 exports.registerUser = async(req,res)=>{
     const {username, email, password} = req.body;
@@ -24,5 +25,37 @@ exports.registerUser = async(req,res)=>{
         res.status(201).json({message: "User registered successfully", user});
     }catch(error){
         res.status(500).json({message: "Failed to register user", error: error.message});
+    }
+}
+
+exports.loginUser = async(req,res)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message:"Email and password are required"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).json({message:"Invalid credentials"});
+        }
+
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        );
+
+        res.status(200).json({message:"Logged in successfully", token});
+    }catch(error){
+        res.status(500).json({message:"Failed to log in", error: error.message});
     }
 }
